23c3e65cfb33e7d5c603e8965bc8a94857bb1c0a
Axionize <154778082+Axionize@users.noreply.github.com>
2024-10-08 20:50:08 -0400

    Add new toggle.self, other, and global permisssions

--- src/main/java/me/caseload/knockbacksync/command/subcommand/ToggleSubcommand.java
+++ src/main/java/me/caseload/knockbacksync/command/subcommand/ToggleSubcommand.java
@@ -1,6 +1,7 @@
 package me.caseload.knockbacksync.command.subcommand;
 
 import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.CommandPermission;
 import dev.jorel.commandapi.arguments.PlayerArgument;
 import me.caseload.knockbacksync.KnockbackSync;
 import me.caseload.knockbacksync.manager.ConfigManager;
@@ -8,6 +9,7 @@ import me.caseload.knockbacksync.manager.PlayerData;
 import me.caseload.knockbacksync.manager.PlayerDataManager;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.Listener;
 
 import java.util.UUID;
@@ -16,7 +18,7 @@ public class ToggleSubcommand implements Listener {
 
     public CommandAPICommand getCommand() {
         return new CommandAPICommand("toggle")
-                .withPermission("knockbacksync.toggle")
+                .withPermission(CommandPermission.NONE)
                 .withOptionalArguments(new PlayerArgument("target"))
                 .executes((sender, args) -> {
                     ConfigManager configManager = KnockbackSync.getInstance().getConfigManager();
@@ -24,40 +26,61 @@ public class ToggleSubcommand implements Listener {
                     String message;
 
                     if (target == null) {
-                        boolean toggledState = !configManager.isToggled();
-                        configManager.setToggled(toggledState);
+                        // Global toggle
+                        if (sender.hasPermission("knockbacksync.toggle.global")) {
+                            boolean toggledState = !configManager.isToggled();
+                            configManager.setToggled(toggledState);
 
-                        KnockbackSync.getInstance().getConfig().set("enabled", toggledState);
-                        KnockbackSync.getInstance().saveConfig();
+                            KnockbackSync.getInstance().getConfig().set("enabled", toggledState);
+                            KnockbackSync.getInstance().saveConfig();
 
-                        message = ChatColor.translateAlternateColorCodes('&',
-                                toggledState ? configManager.getEnableMessage() : configManager.getDisableMessage()
-                        );
-                    }
-                    else {
-                        UUID uuid = target.getUniqueId();
-
-                        if (PlayerDataManager.shouldExempt(uuid)) {
                             message = ChatColor.translateAlternateColorCodes('&',
-                                    configManager.getPlayerIneligibleMessage()
-                            ).replace("%player%", target.getName());
-
-                            sender.sendMessage(message);
-                            return;
+                                    toggledState ? configManager.getEnableMessage() : configManager.getDisableMessage()
+                            );
+                        } else {
+                            message = ChatColor.RED + "You don't have permission to toggle the global setting.";
                         }
-
-                        boolean hasPlayerData = PlayerDataManager.containsPlayerData(uuid);
-                        if (hasPlayerData)
-                            PlayerDataManager.removePlayerData(uuid);
-                        else
-                            PlayerDataManager.addPlayerData(uuid, new PlayerData(target));
-
-                        message = ChatColor.translateAlternateColorCodes('&',
-                                hasPlayerData ? configManager.getPlayerDisableMessage() : configManager.getPlayerEnableMessage()
-                        ).replace("%player%", target.getName());
+                        sender.sendMessage(message);
+                    } else {
+                        // Player-specific toggle
+                        if (!configManager.isToggled()) {
+                            message = ChatColor.RED + "Knockbacksync is currently disabled on this server. Contact your server administrator for more information.";
+                            sender.sendMessage(message);
+                        } else if (sender instanceof Player && sender.equals(target) && sender.hasPermission("knockbacksync.toggle.self")) {
+                            togglePlayerKnockback(target, configManager, sender);
+                        } else if (sender.hasPermission("knockbacksync.toggle.other")) {
+                            togglePlayerKnockback(target, configManager, sender);
+                        } else {
+                            message = ChatColor.RED + "You don't have permission to toggle knockback for " +
+                                    (sender.equals(target) ? "yourself" : "other players") + ".";
+                            sender.sendMessage(message);
+                        }
                     }
-
-                    sender.sendMessage(message);
                 });
+    }
+
+    private void togglePlayerKnockback(Player target, ConfigManager configManager, CommandSender sender) {
+        UUID uuid = target.getUniqueId();
+
+        if (PlayerDataManager.shouldExempt(uuid)) {
+            String message = ChatColor.translateAlternateColorCodes('&',
+                    configManager.getPlayerIneligibleMessage()
+            ).replace("%player%", target.getName());
+
+            sender.sendMessage(message);
+            return;
+        }
+
+        boolean hasPlayerData = PlayerDataManager.containsPlayerData(uuid);
+        if (hasPlayerData)
+            PlayerDataManager.removePlayerData(uuid);
+        else
+            PlayerDataManager.addPlayerData(uuid, new PlayerData(target));
+
+        String message = ChatColor.translateAlternateColorCodes('&',
+                hasPlayerData ? configManager.getPlayerDisableMessage() : configManager.getPlayerEnableMessage()
+        ).replace("%player%", target.getName());
+
+        sender.sendMessage(message);
     }
 }
