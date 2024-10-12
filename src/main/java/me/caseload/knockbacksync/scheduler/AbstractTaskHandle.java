94201ae9f62562d5190b145f3e72d8f2f0a05495
Axionize <154778082+Axionize@users.noreply.github.com>
2024-10-12 04:50:03 -0400

    Implement tasks on fabric

--- src/main/java/me/caseload/knockbacksync/scheduler/AbstractTaskHandle.java
+++ src/main/java/me/caseload/knockbacksync/scheduler/AbstractTaskHandle.java
@@ -1,6 +1,7 @@
 package me.caseload.knockbacksync.scheduler;
 
 import io.papermc.paper.threadedregions.scheduler.ScheduledTask;
+import lombok.Getter;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitTask;
 import org.jetbrains.annotations.NotNull;
@@ -9,6 +10,8 @@ public class AbstractTaskHandle {
     private BukkitTask bukkitTask;
     private ScheduledTask scheduledTask;
     private Runnable cancellationTask;
+    @Getter
+    private boolean cancelled = false;
 
     public AbstractTaskHandle(Runnable cancellationTask) {
         this.cancellationTask = cancellationTask;
@@ -22,12 +25,9 @@ public class AbstractTaskHandle {
         this.scheduledTask = scheduledTask;
     }
 
+    // Should never be called on Fabric
     public Plugin getOwner() {
         return this.bukkitTask != null ? this.bukkitTask.getOwner() : this.scheduledTask.getOwningPlugin();
-    }
-
-    public boolean isCancelled() {
-        return this.bukkitTask != null ? this.bukkitTask.isCancelled() : this.scheduledTask.isCancelled();
     }
 
     public void cancel() {
@@ -38,6 +38,6 @@ public class AbstractTaskHandle {
         } else {
             this.scheduledTask.cancel();
         }
-
+        cancelled = true;
     }
 }
--- src/main/java/me/caseload/knockbacksync/scheduler/FabricSchedulerAdapter.java
+++ src/main/java/me/caseload/knockbacksync/scheduler/FabricSchedulerAdapter.java
@@ -1,83 +1,108 @@
-//package me.caseload.knockbacksync.scheduler;
-//
-//import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-//import net.minecraft.server.MinecraftServer;
-//
-//public class FabricSchedulerAdapter implements SchedulerAdapter {
-//    private final Object plugin; // You might need to change this to whatever represents your mod in Fabric
-//    private final MinecraftServer server;
-//
-//    public FabricSchedulerAdapter(Object plugin, MinecraftServer server) {
-//        this.plugin = plugin;
-//        this.server = server;
-//    }
-//
-//    @Override
-//    public AbstractTaskHandle runTask(Runnable task) {
-//        return new AbstractTaskHandle(ServerTickEvents.END_SERVER_TICK.register(server -> task.run()));
-//    }
-//
-//    @Override
-//    public AbstractTaskHandle runTaskAsynchronously(Runnable task) {
-//        Thread thread = new Thread(task);
-//        thread.start();
-//        return new AbstractTaskHandle(() -> thread.interrupt());
-//    }
-//
-//    @Override
-//    public AbstractTaskHandle runTaskLater(Runnable task, long delayTicks) {
-//        long targetTick = server.getTicks() + delayTicks;
-//        ServerTickEvents.EndTick event = server -> {
-//            if (server.getTicks() >= targetTick) {
-//                task.run();
-//                ServerTickEvents.END_SERVER_TICK.unregister(this);
-//            }
-//        };
-//        ServerTickEvents.END_SERVER_TICK.register(event);
-//        return new AbstractTaskHandle(() -> ServerTickEvents.END_SERVER_TICK.unregister(event));
-//    }
-//
-//    @Override
-//    public AbstractTaskHandle runTaskTimer(Runnable task, long delayTicks, long periodTicks) {
-//        long nextRunTick = server.getTicks() + delayTicks;
-//        ServerTickEvents.EndTick event = server -> {
-//            if (server.getTicks() >= nextRunTick) {
-//                task.run();
-//                nextRunTick = server.getTicks() + periodTicks;
-//            }
-//        };
-//        ServerTickEvents.END_SERVER_TICK.register(event);
-//        return new AbstractTaskHandle(() -> ServerTickEvents.END_SERVER_TICK.unregister(event));
-//    }
-//
-//    @Override
-//    public AbstractTaskHandle runTaskLaterAsynchronously(Runnable task, long delay) {
-//        Thread thread = new Thread(() -> {
-//            try {
-//                Thread.sleep(delay * 50); // Convert ticks to milliseconds
-//                task.run();
-//            } catch (InterruptedException e) {
-//                // Handle interruption
-//            }
-//        });
-//        thread.start();
-//        return new AbstractTaskHandle(() -> thread.interrupt());
-//    }
-//
-//    @Override
-//    public AbstractTaskHandle runTaskTimerAsynchronously(Runnable task, long delay, long period) {
-//        Thread thread = new Thread(() -> {
-//            try {
-//                Thread.sleep(delay * 50); // Convert ticks to milliseconds
-//                while (!Thread.currentThread().isInterrupted()) {
-//                    task.run();
-//                    Thread.sleep(period * 50); // Convert ticks to milliseconds
-//                }
-//            } catch (InterruptedException e) {
-//                // Handle interruption
-//            }
-//        });
-//        thread.start();
-//        return new AbstractTaskHandle(() -> thread.interrupt());
-//    }
-//}
+package me.caseload.knockbacksync.scheduler;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.server.MinecraftServer;
+
+public class FabricSchedulerAdapter implements SchedulerAdapter {
+    private final Object plugin;
+    private final MinecraftServer server;
+    private final List<ScheduledTask> taskList;
+
+    public FabricSchedulerAdapter(Object plugin, MinecraftServer server) {
+        this.plugin = plugin;
+        this.server = server;
+        this.taskList = new ArrayList<>();
+        ServerTickEvents.END_SERVER_TICK.register(this::handleTasks);
+    }
+
+    private void handleTasks(MinecraftServer server) {
+        Iterator<ScheduledTask> iterator = taskList.iterator();
+        while (iterator.hasNext()) {
+            ScheduledTask task = iterator.next();
+            if (server.getTickCount() >= task.nextRunTick) {
+                task.task.run();
+                if (task.isPeriodic) {
+                    task.nextRunTick = server.getTickCount() + task.period;
+                } else {
+                    iterator.remove();
+                }
+            }
+        }
+    }
+
+    @Override
+    public AbstractTaskHandle runTask(Runnable task) {
+        ScheduledTask scheduledTask = new ScheduledTask(task, server.getTickCount(), 0, false);
+        taskList.add(scheduledTask);
+        return new AbstractTaskHandle(() -> taskList.remove(scheduledTask));
+    }
+
+    @Override
+    public AbstractTaskHandle runTaskAsynchronously(Runnable task) {
+        Thread thread = new Thread(task);
+        thread.start();
+        return new AbstractTaskHandle(() -> thread.interrupt());
+    }
+
+    @Override
+    public AbstractTaskHandle runTaskLater(Runnable task, long delayTicks) {
+        ScheduledTask scheduledTask = new ScheduledTask(task, server.getTickCount() + delayTicks, 0, false);
+        taskList.add(scheduledTask);
+        return new AbstractTaskHandle(() -> taskList.remove(scheduledTask));
+    }
+
+    @Override
+    public AbstractTaskHandle runTaskTimer(Runnable task, long delayTicks, long periodTicks) {
+        ScheduledTask scheduledTask = new ScheduledTask(task, server.getTickCount() + delayTicks, periodTicks, true);
+        taskList.add(scheduledTask);
+        return new AbstractTaskHandle(() -> taskList.remove(scheduledTask));
+    }
+
+    @Override
+    public AbstractTaskHandle runTaskLaterAsynchronously(Runnable task, long delay) {
+        Thread thread = new Thread(() -> {
+            try {
+                Thread.sleep(delay * 50); // Convert ticks to milliseconds
+                task.run();
+            } catch (InterruptedException e) {
+                // Handle interruption
+            }
+        });
+        thread.start();
+        return new AbstractTaskHandle(() -> thread.interrupt());
+    }
+
+    @Override
+    public AbstractTaskHandle runTaskTimerAsynchronously(Runnable task, long delay, long period) {
+        Thread thread = new Thread(() -> {
+            try {
+                Thread.sleep(delay * 50); // Convert ticks to milliseconds
+                while (!Thread.currentThread().isInterrupted()) {
+                    task.run();
+                    Thread.sleep(period * 50); // Convert ticks to milliseconds
+                }
+            } catch (InterruptedException e) {
+                // Handle interruption
+            }
+        });
+        thread.start();
+        return new AbstractTaskHandle(() -> thread.interrupt());
+    }
+
+    private static class ScheduledTask {
+        final Runnable task;
+        long nextRunTick;
+        final long period;
+        final boolean isPeriodic;
+
+        ScheduledTask(Runnable task, long nextRunTick, long period, boolean isPeriodic) {
+            this.task = task;
+            this.nextRunTick = nextRunTick;
+            this.period = period;
+            this.isPeriodic = isPeriodic;
+        }
+    }
+}
