ae3df3bf985c9ea73cf7f0451c06c935d763ee38
Axionize <154778082+Axionize@users.noreply.github.com>
2024-10-16 03:00:17 -0400

    MultiPaper support

--- bukkit/src/main/java/me/caseload/knockbacksync/listener/bukkit/BukkitPlayerDamageListener.java
+++ bukkit/src/main/java/me/caseload/knockbacksync/listener/bukkit/BukkitPlayerDamageListener.java
@@ -2,6 +2,7 @@ package me.caseload.knockbacksync.listener.bukkit;
 
 import me.caseload.knockbacksync.listener.PlayerDamageListener;
 import me.caseload.knockbacksync.player.BukkitPlayer;
+import me.caseload.knockbacksync.util.MultiLibUtil;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,7 +13,8 @@ public class BukkitPlayerDamageListener extends PlayerDamageListener implements 
     @EventHandler(ignoreCancelled = true)
     public void onPlayerDamage(EntityDamageByEntityEvent event) {
         if ((event.getEntity() instanceof Player victim) && (event.getDamager() instanceof Player attacker))
-            onPlayerDamage(new BukkitPlayer(victim), new BukkitPlayer(attacker));
-
+            if (!MultiLibUtil.isExternalPlayer(victim)) {
+                onPlayerDamage(new BukkitPlayer(victim), new BukkitPlayer(attacker));
+            }
     }
 }
